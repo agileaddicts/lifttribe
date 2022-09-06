b6a0ec1db954f16eb5055d40d441d96be4939c54
Sebastian Göttschkes <sebastian@agileaddicts.com>
2022-09-06 19:20:29 +0200

    Feat: Add basic design with logo

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,10 @@ module.exports = {
   content: ["./assets/js/**/*.js", "./lib/*_web.ex", "./lib/*_web/**/*.*ex"],
   darkMode: "class",
   theme: {
+    colors: {
+      'midnight': '#23252e',
+      'orange': '#ff9045'
+    }
   },
   plugins: [],
 };
