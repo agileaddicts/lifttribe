ecbdc371a0f9e6b412495e42f8dfac3aebae0999
Sebastian Göttschkes <sebastian@agileaddicts.com>
2022-09-07 16:56:31 +0200

    Feat: Add design to form and current stats table

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,22 @@
+const colors = require('tailwindcss/colors');
+
 module.exports = {
   content: ["./assets/js/**/*.js", "./lib/*_web.ex", "./lib/*_web/**/*.*ex"],
   darkMode: "class",
   theme: {
     colors: {
-      'midnight': '#23252e',
-      'orange': '#ff9045'
+      transparent: 'transparent',
+      current: 'currentColor',
+      black: colors.black,
+      white: colors.white,
+      slate: colors.slate,
+      indigo: colors.indigo,
+      // custom colors
+      midnight: '#23252e',
+      orange: '#ff9045'
     }
   },
-  plugins: [],
+  plugins: [
+    require('@tailwindcss/forms')
+  ]
 };
