0b0cd661b764220485fe17d2a577cc24111626d4
Sebastian Göttschkes <sebastian@agileaddicts.com>
2022-09-30 06:08:23 +0200

    Chore: Add test for /auth/send_auth_code

--- test/lifttribe_web/controllers/auth_controller_test.exs
+++ test/lifttribe_web/controllers/auth_controller_test.exs
@@ -1,8 +1,24 @@
 defmodule LifttribeWeb.AuthControllerTest do
   use LifttribeWeb.ConnCase
 
+  import Lifttribe.Factory
+
   test "GET /auth/login", %{conn: conn} do
     conn = get(conn, "/auth/login")
     assert html_response(conn, 200) =~ "Login"
+  end
+
+  test "POST /auth/send_auth_code with existing email", %{conn: conn} do
+    athlete = insert!(:athlete)
+
+    conn = post(conn, "/auth/send_auth_code", email: athlete.email)
+    assert redirected_to(conn) == Routes.page_path(conn, :index)
+    assert get_flash(conn, :info)
+  end
+
+  test "POST /auth/send_auth_code with non-existing email", %{conn: conn} do
+    conn = post(conn, "/auth/send_auth_code", email: "nonexisting@lifttribe.local")
+    assert redirected_to(conn) == Routes.page_path(conn, :index)
+    assert get_flash(conn, :info)
   end
 end
