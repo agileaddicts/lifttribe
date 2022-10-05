4d41942b5e6a0c4c0c335ff198abf73eb2a16d4e
Sebastian Göttschkes <sebastian@agileaddicts.com>
2022-10-05 09:26:26 +0200

    Feat: Make footer dark

--- lib/lifttribe_web/components/button_component.ex
+++ lib/lifttribe_web/components/button_component.ex
@@ -3,7 +3,7 @@ defmodule LifttribeWeb.Components.ButtonComponent do
 
   def render(assigns) do
     ~H"""
-    <button class="inline-flex items-center justify-center w-full px-4 py-2 text-sm font-semibold text-white rounded-full bg-orange group focus:outline-none focus-visible:outline-2 focus-visible:outline-offset-2 hover:text-slate-100 hover:bg-orange active:bg-orange active:text-orange focus-visible:outline-orange" type={@type}>
+    <button class="inline-flex items-center justify-center w-full px-4 py-2 text-sm font-semibold text-white rounded-full bg-orange group  hover:text-slate-100 hover:opacity-80" type={@type}>
       <span><%= @text %></span>
     </button>
     """
